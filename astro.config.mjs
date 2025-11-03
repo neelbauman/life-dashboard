52fd1aba06b6f5e185b273c4a08ab097f7fb0968
Neel Bauman <neel.bauman@example.com>
2025-11-03 20:07:51 +0900

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://neelbauman.github.io',
-	base: 'life-dashboard'
+	base: '/life-dashboard'
 });
